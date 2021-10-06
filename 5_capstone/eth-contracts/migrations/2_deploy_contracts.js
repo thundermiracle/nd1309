1cd26bebefd314193909e786f0e1e6f863c882bb
thundermiracle <cissoliu@gmail.com>
2021-10-06 21:02:22 +0900

    test: SquareVerifier.sol

--- 5_capstone/eth-contracts/migrations/2_deploy_contracts.js
+++ 5_capstone/eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 // migrating the appropriate contracts
-// const SquareVerifier = artifacts.require("./SquareVerifier.sol");
+const SquareVerifier = artifacts.require("./SquareVerifier.sol");
 // const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function (deployer) {
-  // deployer.deploy(SquareVerifier);
+  deployer.deploy(SquareVerifier);
   // deployer.deploy(SolnSquareVerifier);
 };
