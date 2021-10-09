b703679559b6d7923163a6b14e405ce5c5713066
thundermiracle <cissoliu@gmail.com>
2021-10-09 18:31:22 +0900

    feat: finish SolnSquareVerifier.sol

--- 5_capstone/eth-contracts/contracts/SolnSquareVerifier.sol
+++ 5_capstone/eth-contracts/contracts/SolnSquareVerifier.sol
@@ -2,59 +2,85 @@
 pragma solidity ^0.8.0;
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./ERC721Mintable.sol";
+import "./SquareVerifier.sol";
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is ERC721TokenRealEstate {
+  using Counters for Counters.Counter;
 
+  // TODO define a solutions struct that can hold an index & an address
+  struct Solution {
+    uint256 tokenId;
+    address to;
+    bool isVerified;
+    bool isMinted;
+  }
 
+  SquareVerifier private verifier;
 
-// TODO define a solutions struct that can hold an index & an address
+  // TODO define an array of the above struct
+  Counters.Counter private solutionIndex;
 
+  // TODO define a mapping to store unique solutions submitted
+  mapping(bytes32 => Solution) private solutions;
 
-// TODO define an array of the above struct
+  constructor(address _verifierAddress) ERC721TokenRealEstate() {
+    verifier = SquareVerifier(_verifierAddress);
+  }
 
+  // TODO Create an event to emit when a solution is added
+  event SolutionAdded(address to, uint256 tokenId);
 
-// TODO define a mapping to store unique solutions submitted
+  function getSolutionKey(
+    uint256[2] memory a,
+    uint256[2][2] memory b,
+    uint256[2] memory c,
+    uint256[2] memory input
+  ) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(a, b, c, input));
+  }
 
+  // TODO Create a function to add the solutions to the array and emit the event
+  function addSolution(
+    uint256[2] memory a,
+    uint256[2][2] memory b,
+    uint256[2] memory c,
+    uint256[2] memory input
+  ) public withNotPaused {
+    bytes32 solutionKey = getSolutionKey(a, b, c, input);
+    require(solutions[solutionKey].to == address(0), "Solution is already exist");
 
+    // verify input
+    require(verifier.verifyTx(a, b, c, input), "Solution cannot be verified");
 
-// TODO Create an event to emit when a solution is added
+    // add to solutions
+    Solution memory solution = Solution({
+      tokenId: solutionIndex.current(),
+      to: msg.sender,
+      isVerified: true,
+      isMinted: false
+    });
+    solutionIndex.increment();
+    solutions[solutionKey] = solution;
 
+    emit SolutionAdded(solution.to, solution.tokenId);
+  }
 
+  // TODO Create a function to mint new NFT only after the solution has been verified
+  //  - make sure the solution is unique (has not been used before)
+  //  - make sure you handle metadata as well as tokenSuplly
+  function mintNFT(
+    uint256[2] memory a,
+    uint256[2][2] memory b,
+    uint256[2] memory c,
+    uint256[2] memory input
+  ) public returns (bool) {
+    bytes32 solutionKey = getSolutionKey(a, b, c, input);
+    Solution memory solution = solutions[solutionKey];
+    require(solution.isVerified, "Token is not verified");
 
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return mint(solution.to, solution.tokenId);
+  }
+}
--- 5_capstone/eth-contracts/migrations/2_deploy_contracts.js
+++ 5_capstone/eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 // migrating the appropriate contracts
 const SquareVerifier = artifacts.require("./SquareVerifier.sol");
-// const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(SquareVerifier);
-  // deployer.deploy(SolnSquareVerifier);
+const ERC721TokenRealEstate = artifacts.require("Erc721TokenRealEstate");
+
+module.exports = async function (deployer) {
+  await deployer.deploy(SquareVerifier);
+  await deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
+  await deployer.deploy(ERC721TokenRealEstate);
 };
